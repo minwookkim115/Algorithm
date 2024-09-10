5f5dc6754f188b2dd5cd747a0b2215e286e90751
minwookkim115 <139411503+minwookkim115@users.noreply.github.com>
2024-09-10 11:30:51 +0900

    [level 1] Title: 삼총사, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -2,10 +2,17 @@ from itertools import combinations
 
 def solution(number):
     answer = 0
-    com = combinations(number, 3)
+#     com = combinations(number, 3)
     
-    for val in com:
-        if sum(val) == 0:
-            answer += 1
-            
+#     for val in com:
+#         if sum(val) == 0:
+#             answer += 1
+    
+    l = len(number)
+    for i in range(l - 2):
+        for j in range(i + 1, l - 1):
+            for k in range(j + 1, l):
+                if number[i] + number[j] + number[k] == 0:
+                    answer += 1
+    
     return answer
