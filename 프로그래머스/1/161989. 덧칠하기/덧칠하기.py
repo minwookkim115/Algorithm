0bfb3f55cb29d24f565d3e408f6dc4ff31ba5606
minwookkim115 <139411503+minwookkim115@users.noreply.github.com>
2024-12-28 16:34:51 +0900

    [level 1] Title: 덧칠하기, Time: 3.76 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,11 @@
+def solution(n, m, section):
+    answer = 0
+    
+    e = 0
+    
+    for s in section:
+        if s > e:
+            answer += 1
+            e = s + m - 1
+    
+    return answer
